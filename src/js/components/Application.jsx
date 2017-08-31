408b14af6461cd2c4227ae58ac02976492e4af6c
Karol Majta <karolmajta@gmail.com>
2017-09-01 01:36:09 +0200

    Application Scaffold
    Contains proper build pipeline with cleaning, building (assets, less,
    js/jsx and ejs to html), testing using jest and development server.

---
+++ src/js/components/Application.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Application (props, context) {
+    return <img src="images/logo.png" alt="MovieDB Demo" className="logo" />;
+}
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Application from './components/Application.jsx';
+
+ReactDOM.render(React.createElement(Application, {}), window.document.getElementById('app'));
